1b43867231b89dc07cceecfadc639f3d0cce669a
jcodewriter <jcodewriter@gmail.com>
2020-12-08 18:13:29 +0200

    update product view

--- application/views/product/_product_item.php
+++ application/views/product/_product_item.php
@@ -15,6 +15,19 @@
             <?php endif; ?>
         </div>
         <div class="row-custom item-details">
+            <?php $user = get_user($product->user_id); ?>
+            <?php if (get_location($product)) : ?>
+                <a href="<?php echo generate_product_url($product); ?>" name="ads_link" class="product-location">
+                    <span class="location-icon">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
+                        </svg>
+                    </span>
+                    <span class="location-text">
+                        <?php echo get_location($product); ?>
+                    </span>
+                </a>
+            <?php endif; ?>
             <h3 class="product-title">
                 <?php if (is_arabic($product->title)) : ?>
                     <a href="<?php echo generate_product_url($product); ?>" name="ads_link">
@@ -34,12 +47,16 @@
                     } ?>
                 </a>
                 <?php if (!$profile_avatar) : ?>
-                    <?php $user = get_user($product->user_id); ?>
-                    <a href="<?php echo lang_base_url() . 'profile/' . $user->slug; ?>" class="userinfo__wrapper">
-                        <img src="<?php echo get_user_avatar($user); ?>" alt="User" style="width: 35px; height: 35px; border-radius: 50%" />
-                        <span class="last-seen <?php echo (is_user_online($user->last_seen)) ? 'last-seen-online' : ''; ?>"> <i class="icon-circle"></i></span>
-                    </a>
-                <?php endif ?>
+                    <div class="d-flex align-items-center">
+                        <?php if (!$user->is_private) : ?>
+                            <span class="store-mark">Store</span>
+                        <?php endif; ?>
+                        <a href="<?php echo lang_base_url() . 'profile/' . $user->slug; ?>" class="userinfo__wrapper">
+                            <img src="<?php echo get_user_avatar($user); ?>" alt="User" style="width: 30px; height: 30px; border-radius: 50%" />
+                            <span class="last-seen <?php echo (is_user_online($user->last_seen)) ? 'last-seen-online' : ''; ?>"> <i class="icon-circle"></i></span>
+                        </a>
+                    </div>
+                <?php endif; ?>
             </div>
             <a href="<?php echo generate_product_url($product); ?>" name="ads_link" class="item-meta">
                 <?php $this->load->view('product/_price_product_item', ['product' => $product]); ?>
--- application/views/product/_product_item_th_list.php
+++ application/views/product/_product_item_th_list.php
@@ -15,6 +15,19 @@
             <?php endif; ?>
         </div>
         <div class="row-custom item-details product-item-th-list-text" style="">
+            <?php $user = get_user($product->user_id); ?>
+            <?php if (get_location($product)) : ?>
+                <a href="<?php echo generate_product_url($product); ?>" name="ads_link" class="product-location">
+                    <span class="location-icon">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
+                        </svg>
+                    </span>
+                    <span class="location-text">
+                        <?php echo get_location($product); ?>
+                    </span>
+                </a>
+            <?php endif; ?>
             <div class="product-title">
                 <?php if (is_arabic($product->title)) : ?>
                     <a href="<?php echo generate_product_url($product); ?>" name="ads_link"><?php echo html_escape($product->title); ?> </a>
@@ -22,16 +35,6 @@
                     <a href="<?php echo generate_product_url($product); ?>" name="ads_link" style="direction: rtl"><?php echo html_escape($product->title); ?></a>
                 <?php endif; ?>
             </div>
-            <a href="<?php echo generate_product_url($product); ?>" name="ads_link" class="product-location">
-                <span class="location-icon">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                        <path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
-                    </svg>
-                </span>
-                <span class="location-text">
-                    <?php echo get_location($product); ?>
-                </span>
-            </a>
 
             <!--stars-->
             <div class="product-moreinfo__wrapper">
@@ -40,12 +43,17 @@
                         $this->load->view('partials/_review_stars', ['review' => $product->rating]);
                     } ?>
                 </a>
-                <?php $user = get_user($product->user_id); ?>
-                <a href="<?php echo lang_base_url() . 'profile/' . $user->slug; ?>" class="userinfo__wrapper">
-                    <img src="<?php echo get_user_avatar($user); ?>" alt="User" style="width: 35px; height: 35px; border-radius: 50%" />
-                    <span class="last-seen <?php echo (is_user_online($user->last_seen)) ? 'last-seen-online' : ''; ?>"> <i class="icon-circle"></i></span>
-                </a>
+                <div class="d-flex align-items-center">
+                    <?php if (!$user->is_private) : ?>
+                        <span class="store-mark">Store</span>
+                    <?php endif; ?>
+                    <a href="<?php echo lang_base_url() . 'profile/' . $user->slug; ?>" class="userinfo__wrapper">
+                        <img src="<?php echo get_user_avatar($user); ?>" alt="User" style="width: 30px; height: 30px; border-radius: 50%" />
+                        <span class="last-seen <?php echo (is_user_online($user->last_seen)) ? 'last-seen-online' : ''; ?>"> <i class="icon-circle"></i></span>
+                    </a>
+                </div>
             </div>
+
             <a href="<?php echo generate_product_url($product); ?>" name="ads_link" class="item-meta">
                 <?php $this->load->view('product/_price_product_item', ['product' => $product]); ?>
                 <span style="float: right;"><?php echo time_ago($product->created_at); ?></span>
