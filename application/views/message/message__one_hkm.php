0903f5bd706676b72966ca5d1cec01de199949bb
jcodewriter <jcodewriter@gmail.com>
2020-12-14 21:44:42 +0200

    Update message__one_hkm.php

--- application/views/message/message__one_hkm.php
+++ application/views/message/message__one_hkm.php
@@ -693,11 +693,11 @@
                                                             </div>
                                                             <div class="user-message">
                                                                 <?php if( substr(html_escape($item->message), 0, 10 ) === "emoticons/" ) { ?> 
-                                                                <div class="message-text">
+                                                                <div class="message-text" style="padding: 0 !important;">
                                                                     <img src="https://www.zolmarket.com/uploads/profile/<?php echo html_escape($item->message); ?>" style="width:50px"  class="" >
                                                                 </div>
                                                                 <?php }else{ ?>
-                                                                <div class="message-text">
+                                                                <div class="message-text" style="padding: 0 !important;">
                                                                     <img src="https://www.zolmarket.com/uploads/profile/<?php echo html_escape($item->message); ?>"   class="img-thumbnail" >
                                                                 </div>
                                                                 <?php } ?>
@@ -730,11 +730,11 @@
                                                     <div class="message-list-item-row-sent">
                                                         <div class="user-message">
                                                             <?php if( substr(html_escape($item->message), 0, 10 ) === "emoticons/" ) { ?> 
-                                                            <div class="message-text" style="padding: 0 !important;">
+                                                            <div class="message-text">
                                                                 <img src="https://www.zolmarket.com/uploads/profile/<?php echo html_escape($item->message); ?>" style="width:50px"  class="" >
                                                             </div>
                                                             <?php }else{ ?>
-                                                            <div class="message-text" style="padding: 0 !important;">
+                                                            <div class="message-text">
                                                                 <img src="https://www.zolmarket.com/uploads/profile/<?php echo html_escape($item->message); ?>"   class="img-thumbnail" >
                                                             </div>
                                                             <?php } ?>
