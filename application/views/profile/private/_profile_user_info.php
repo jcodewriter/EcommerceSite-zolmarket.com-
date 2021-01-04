46318f619606cbdd9a1b0ffa64936fb540534b98
jcodewriter <jcodewriter@gmail.com>
2021-01-05 00:59:53 +0200

    fixed profile page

--- application/views/profile/private/_profile_user_info.php
+++ application/views/profile/private/_profile_user_info.php
@@ -91,33 +91,35 @@
                 <span class="info"><i class="icon-map-marker"></i><?php echo get_location($user); ?></span>
             <?php endif; ?>
         </div>
-        <div class="social hidden-md-down">
-            <ul>
-                <?php if (!empty($user->facebook_url)) : ?>
-                    <li><a href="<?php echo $user->facebook_url; ?>" target="_blank"><i class="icon-facebook"></i></a></li>
-                <?php endif; ?>
-                <?php if (!empty($user->twitter_url)) : ?>
-                    <li><a href="<?php echo $user->twitter_url; ?>" target="_blank"><i class="icon-twitter"></i></a></li>
-                <?php endif; ?>
-                <?php if (!empty($user->instagram_url)) : ?>
-                    <li><a href="<?php echo $user->instagram_url; ?>" target="_blank"><i class="icon-instagram"></i></a></li>
-                <?php endif; ?>
-                <?php if (!empty($user->pinterest_url)) : ?>
-                    <li><a href="<?php echo $user->pinterest_url; ?>" target="_blank"><i class="icon-pinterest"></i></a></li>
-                <?php endif; ?>
-                <?php if (!empty($user->linkedin_url)) : ?>
-                    <li><a href="<?php echo $user->linkedin_url; ?>" target="_blank"><i class="icon-linkedin"></i></a></li>
-                <?php endif; ?>
-                <?php if (!empty($user->vk_url)) : ?>
-                    <li><a href="<?php echo $user->vk_url; ?>" target="_blank"><i class="icon-vk"></i></a></li>
-                <?php endif; ?>
-                <?php if (!empty($user->youtube_url)) : ?>
-                    <li><a href="<?php echo $user->youtube_url; ?>" target="_blank"><i class="icon-youtube"></i></a></li>
-                <?php endif; ?>
-                <?php if ($this->general_settings->rss_system == 1 && $user->show_rss_feeds == 1 && get_user_products_count($user->slug) > 0) : ?>
-                    <li><a href="<?php echo lang_base_url() . "rss/seller/" . $user->slug; ?>" target="_blank"><i class="icon-rss"></i></a></li>
-                <?php endif; ?>
-            </ul>
+        <div class="row-custom profile-buttons">
+            <div class="social hidden-md-down">
+                <ul>
+                    <?php if (!empty($user->facebook_url)) : ?>
+                        <li><a href="<?php echo $user->facebook_url; ?>" target="_blank"><i class="icon-facebook"></i></a></li>
+                    <?php endif; ?>
+                    <?php if (!empty($user->twitter_url)) : ?>
+                        <li><a href="<?php echo $user->twitter_url; ?>" target="_blank"><i class="icon-twitter"></i></a></li>
+                    <?php endif; ?>
+                    <?php if (!empty($user->instagram_url)) : ?>
+                        <li><a href="<?php echo $user->instagram_url; ?>" target="_blank"><i class="icon-instagram"></i></a></li>
+                    <?php endif; ?>
+                    <?php if (!empty($user->pinterest_url)) : ?>
+                        <li><a href="<?php echo $user->pinterest_url; ?>" target="_blank"><i class="icon-pinterest"></i></a></li>
+                    <?php endif; ?>
+                    <?php if (!empty($user->linkedin_url)) : ?>
+                        <li><a href="<?php echo $user->linkedin_url; ?>" target="_blank"><i class="icon-linkedin"></i></a></li>
+                    <?php endif; ?>
+                    <?php if (!empty($user->vk_url)) : ?>
+                        <li><a href="<?php echo $user->vk_url; ?>" target="_blank"><i class="icon-vk"></i></a></li>
+                    <?php endif; ?>
+                    <?php if (!empty($user->youtube_url)) : ?>
+                        <li><a href="<?php echo $user->youtube_url; ?>" target="_blank"><i class="icon-youtube"></i></a></li>
+                    <?php endif; ?>
+                    <?php if ($this->general_settings->rss_system == 1 && $user->show_rss_feeds == 1 && get_user_products_count($user->slug) > 0) : ?>
+                        <li><a href="<?php echo lang_base_url() . "rss/seller/" . $user->slug; ?>" target="_blank"><i class="icon-rss"></i></a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
