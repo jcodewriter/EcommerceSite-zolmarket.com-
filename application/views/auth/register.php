ccf264bd5290827dc95433a5c6a0d0892d21a3e5
jcodewriter <jcodewriter@gmail.com>
2021-02-01 18:02:44 -0800

    styling

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -105,7 +105,7 @@
 <script>
 $(document).ready(function(){
     $("button").click(function(){
-        $("@imgUploader").parent().find("p").remove();
+        $("#imgUploader").parent().find("p").remove();
         if($("#imgUploader").val() == ''){
             // $('#imgadshoww').css({'border':'2px solid #dc354566'});
             $("html, body").animate({
